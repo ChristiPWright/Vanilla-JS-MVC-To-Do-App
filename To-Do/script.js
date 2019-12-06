3df8f79f98d418017d4396ca9ef0a4980c34410f
Ubuntu <ubuntu@ip-172-31-17-0.us-east-2.compute.internal>
2019-12-06 01:07:34 +0000

    looking for the difference to bugfix

--- To-Do/script.js
+++ To-Do/script.js
@@ -1,257 +1,245 @@
-
+/**
+ * @class Model
+ *
+ * Manages the data of the application.
+ */
 class Model {
-  
-  //updated constructor from this.todos = array of object dummy data
   constructor() {
-      this.todos = JSON.parse(localStorage.getItem('todos')) || []
+    this.todos = JSON.parse(localStorage.getItem('todos')) || []
   }
-  
-  //bind event handlers
-  bindTodoListChanges(callback){
-      this.onTodoListChanged = callback
+
+  bindTodoListChanged(callback) {
+    this.onTodoListChanged = callback
   }
-  
-    //add private method to update localstorage value
-  privateCommit(todos){
-      this.onTodoListChanged(todos)
-      localStorage.setItem('todos', JSON.stringify(todos))
+
+  _commit(todos) {
+    this.onTodoListChanged(todos)
+    localStorage.setItem('todos', JSON.stringify(todos))
   }
-  
-  addToDo(todoText){
-     const todo = {
-         id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
-         text: todoText,
-         complete: false
-     }
-     this.todos.push(todo)
-       
-     this.privateCommit(this.todos)
-   }
-  
-  
-  
+
+  addTodo(todoText) {
+    const todo = {
+      id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+      text: todoText,
+      complete: false,
+    }
+
+    this.todos.push(todo)
+
+    this._commit(this.todos)
+  }
+
   editTodo(id, updatedText) {
-      this.todos = this.todos.map(todo =>
-        todo.id === id ? {id: todo.id, text: updatedText, complete: todo.complete } : todo
-      )
-        this.privateCommit(this.todos)
+    this.todos = this.todos.map(todo =>
+      todo.id === id ? { id: todo.id, text: updatedText, complete: todo.complete } : todo
+    )
+
+    this._commit(this.todos)
   }
-  
-  //localstorage change - 
+
   deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id)
-      
-      this.privateCommit(this.todos)
+    this.todos = this.todos.filter(todo => todo.id !== id)
+
+    this._commit(this.todos)
   }
-  
-  completeTodo(id) {
-      this.todos = this.todos.map(todo =>
-        todo.id === id ? {id: todo.id, text: todo.text, complete: !todo.complete} : todo
-      )
-      this.privateCommit(this.todos)
+
+  toggleTodo(id) {
+    this.todos = this.todos.map(todo =>
+      todo.id === id ? { id: todo.id, text: todo.text, complete: !todo.complete } : todo
+    )
+
+    this._commit(this.todos)
   }
-  
 }
 
+/**
+ * @class View
+ *
+ * Visual representation of the model.
+ */
 class View {
   constructor() {
-      //root element
-      this.app = this.getElement('#root')
-      
-      //title of the app
-      this.title = this.createElement('h1')
-      this.title.textContent = 'Todos'
-      
-      //the form
-      this.form = this.createElement('form')
-      
-      this.input = this.createElement('input')
-      this.input.type = 'text'
-      this.input.placeholder = 'Add todo'
-      this.input.name ='todo'
-      
-      this.submitButton = this.createElement('button')
-      this.submitButton.textContent = 'Submit'
-      
-      //add input and submit
-      this.form.append(this.input, this.submitButton)
-      
-      //visual of todo list
-      this.todoList = this.createElement('ul', 'todo-list')
-      
-      
-      //add title, form, and todo list to app
-      this.app.append(this.title, this.form, this.todoList)
-      
-      //add local storage
-      this._temporaryTodoText = ''
-      this._initLocalListeners()
-  }
-  
-  get privateText(){
-      return this.input.value
-  }
-  
-  privateResetInput(){
-      this.input.value = ''
-  }
-  
+    this.app = this.getElement('#root')
+    this.form = this.createElement('form')
+    this.input = this.createElement('input')
+    this.input.type = 'text'
+    this.input.placeholder = 'Add todo'
+    this.input.name = 'todo'
+    this.submitButton = this.createElement('button')
+    this.submitButton.textContent = 'Submit'
+    this.form.append(this.input, this.submitButton)
+    this.title = this.createElement('h1')
+    this.title.textContent = 'Todos'
+    this.todoList = this.createElement('ul', 'todo-list')
+    this.app.append(this.title, this.form, this.todoList)
 
-  createElement(tag, className){
-      const element = document.createElement(tag)
-      if(className) element.classList.add(className)
-      
-      return element
+    this._temporaryTodoText = ''
+    this._initLocalListeners()
   }
-  
-  getElement(selector){
-      const element = document.querySelector(selector)
-      
-      return element
+
+  get _todoText() {
+    return this.input.value
   }
-  
-  displayTodo(todos){
-      //delete all nodes
-      while (this.todoList.firstChild){
-          this.todoList.removeChild(this.todoList.firstChild)
-      }
-      //show default mssg
-      if(todos.length === 0){
-          const p = this.createElement('p')
-          p.textContent = "Nothing to do! Add a task?"
-          this.todoList.append(p)
-      } else {
-          //create todo item
-          todos.forEach(todo => {
-              const li = this.createElement('li')
-              li.id = todo.id
-              
-              //Each todo checkbox
-              const checkbox = this.createElement('input')
-              checkbox.type = "checkbox"
-              checkbox.checked = todo.complete
-              
-              //todo item in span tag thats editable
-              const span = this.createElement('span')
-              span.contentEditable = true
-              span.classList.add('editable')
-              
-              //if todo is complete it has a strikethrough
-              if(todo.complete){
-                  const strike = this.createElement('s')
-                  strike.textContent = todo.text
-                  span.append(strike)
-              } else {
-                  span.textContent = todo.text
-              }
-              
-              //todos have a delete button
-              const deleteButton = this.createElement('button', 'delete')
-              deleteButton.textContent = "Delete"
-              li.append(checkbox, span, deleteButton)
-              
-              //append nodes to todo list
-              this.todoList.append(li)
-          })
-      }
-      
+
+  _resetInput() {
+    this.input.value = ''
   }
-  
-  //update temporary state
+
+  createElement(tag, className) {
+    const element = document.createElement(tag)
+
+    if (className) element.classList.add(className)
+
+    return element
+  }
+
+  getElement(selector) {
+    const element = document.querySelector(selector)
+
+    return element
+  }
+
+  displayTodos(todos) {
+    // Delete all nodes
+    while (this.todoList.firstChild) {
+      this.todoList.removeChild(this.todoList.firstChild)
+    }
+
+    // Show default message
+    if (todos.length === 0) {
+      const p = this.createElement('p')
+      p.textContent = 'Nothing to do! Add a task?'
+      this.todoList.append(p)
+    } else {
+      // Create nodes
+      todos.forEach(todo => {
+        const li = this.createElement('li')
+        li.id = todo.id
+
+        const checkbox = this.createElement('input')
+        checkbox.type = 'checkbox'
+        checkbox.checked = todo.complete
+
+        const span = this.createElement('span')
+        span.contentEditable = true
+        span.classList.add('editable')
+
+        if (todo.complete) {
+          const strike = this.createElement('s')
+          strike.textContent = todo.text
+          span.append(strike)
+        } else {
+          span.textContent = todo.text
+        }
+
+        const deleteButton = this.createElement('button', 'delete')
+        deleteButton.textContent = 'Delete'
+        li.append(checkbox, span, deleteButton)
+
+        // Append nodes
+        this.todoList.append(li)
+      })
+    }
+
+    // Debugging
+    console.log(todos)
+  }
+
   _initLocalListeners() {
-      this.todoList.addEventListener('input', event => {
-          if(event.target.className === 'editable'){
-              this._temporaryTodoText = event.target.innerText
-          }
-      })
+    this.todoList.addEventListener('input', event => {
+      if (event.target.className === 'editable') {
+        this._temporaryTodoText = event.target.innerText
+      }
+    })
   }
-  
-  //event handlers
-  bindAddTodo(handler){
-      this.form.addEventListener('submit', event => {
-          event.preventDefault()
-          
-          if(this.privateText) {
-              handler(this.privateText)
-              this.privateResetInput()
-          }
-      })
+
+  bindAddTodo(handler) {
+    this.form.addEventListener('submit', event => {
+      event.preventDefault()
+
+      if (this._todoText) {
+        handler(this._todoText)
+        this._resetInput()
+      }
+    })
   }
-  
-  bindEditTodo(handler){
-      this.todoList.addEventListener('focusout', event =>{
-          if (this._temporaryTodoText){
-              const id = parseInt(event.target.parentElement.id)
-              
-              handler(id, this._temporaryTodoText)
-              this._temporaryTodoText = ''
-          }
-      })
+
+  bindDeleteTodo(handler) {
+    this.todoList.addEventListener('click', event => {
+      if (event.target.className === 'delete') {
+        const id = parseInt(event.target.parentElement.id)
+
+        handler(id)
+      }
+    })
   }
-  
-  bindDeleteTodo(handler){
-      this.todoList.addEventListener('click', event => {
-          if(event.target.className === 'delete') {
-              const id = parseInt(event.target.parentElement.id)
-              
-              handler(id)
-          }
-      })
+
+  bindEditTodo(handler) {
+    this.todoList.addEventListener('focusout', event => {
+      if (this._temporaryTodoText) {
+        const id = parseInt(event.target.parentElement.id)
+
+        handler(id, this._temporaryTodoText)
+        this._temporaryTodoText = ''
+      }
+    })
   }
-  
-  bindCompleteTodo(handler){
-      this.todoList.addEventListener('change', event => {
-          if(event.target.type === 'checkbox'){
-              const id = parseInt(event.target.parentElement.id)
-          
-              handler(id)
-          }
-      })
+
+  bindToggleTodo(handler) {
+    this.todoList.addEventListener('change', event => {
+      if (event.target.type === 'checkbox') {
+        const id = parseInt(event.target.parentElement.id)
+
+        handler(id)
+      }
+    })
   }
-  
-  
 }
 
+/**
+ * @class Controller
+ *
+ * Links the user input and the view output.
+ *
+ * @param model
+ * @param view
+ */
 class Controller {
   constructor(model, view) {
     this.model = model
     this.view = view
-    
-    //bind event handlers
+
+    // Explicit this binding
+    this.model.bindTodoListChanged(this.onTodoListChanged)
     this.view.bindAddTodo(this.handleAddTodo)
     this.view.bindEditTodo(this.handleEditTodo)
     this.view.bindDeleteTodo(this.handleDeleteTodo)
-    this.view.bindCompleteTodo(this.handleCompleteTodo)
-    this.model.bindTodoListChanges(this.onTodoListChanged)
-    
-    //display initial todos
+    this.view.bindToggleTodo(this.handleToggleTodo)
+
+    // Display initial todos
     this.onTodoListChanged(this.model.todos)
   }
-  
+
   onTodoListChanged = todos => {
-      this.view.displayTodo(todos)
+    this.view.displayTodos(todos)
   }
-  
+
   handleAddTodo = todoText => {
-      this.model.addTodo(todoText)
+    this.model.addTodo(todoText)
   }
-  
-  handleEditTodo = id => {
-      this.model.deleteTodo(id)
+
+  handleEditTodo = (id, todoText) => {
+    this.model.editTodo(id, todoText)
   }
-  
-  handeDeleteTodo = id => {
-      this.model.deleteTodo(id)
+
+  handleDeleteTodo = id => {
+    this.model.deleteTodo(id)
   }
-  
-  handleCompleteTodo = id => {
-      this.model.completeTodo(id)
+
+  handleToggleTodo = id => {
+    this.model.toggleTodo(id)
   }
-  
 }
 
 const app = new Controller(new Model(), new View())
-
-
-
 
